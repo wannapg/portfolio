fbbb63b7066738b5729a64146d187c5a3f462c73
wannapg <reginakang52@gmail.com>
2020-01-31 16:54:35 +0900

    django

---
+++ portfolio/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
+
+def main(request):
+    return render(request,'main.html')
+
+def courses(request):
+    return render(request,'courses.html')
+
+def projects(request):
+    return render(request,'projects.html')
+
+def dino(request):
+    return render(request,'dino.html')
