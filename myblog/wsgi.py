61b439e0019876c706cfdd0cc7b09d332c01cba3
wannapg <reginakang52@gmail.com>
2020-02-01 14:58:06 +0900

    wgsi configurations rid

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from configurations.wsgi import get_wsgi_application
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
