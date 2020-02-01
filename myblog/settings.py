7ffb7a60f53f9b087eebe432c51ebb6767b62bba
wannapg <reginakang52@gmail.com>
2020-02-01 15:26:25 +0900

    settings import -

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os,json
-import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
